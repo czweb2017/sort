e08fa5f3725beceffc9e95394b6f516967727b79
chenzhuo <chenzhuo2507070974@outlook.com>
2018-10-17 21:07:53 +0800

    'change function files name'

--- test/testPopSort.js
+++ test/testPopSort.js
@@ -1,7 +1,7 @@
-const popSort = require('../src/bubbleSort');
-const insertSort = require('../src/inserSort');
-const selectionSort = require('../src/selectionSort');
-const quickSort = require('../src/quickSort')
+const pop = require('../src/bubble');
+const insert = require('../src/insert');
+const selection = require('../src/selection');
+const quick = require('../src/quick')
 
 const testArr = [34,23,12,5,7,-1,2,-103];
 // const result = popSort(testArr);
